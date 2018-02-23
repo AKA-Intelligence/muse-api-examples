78a53946c57cdda8afcd5833242782cd71412da2
Hooram Nam <nhooram@gmail.com>
2018-02-23 11:31:16 +0900

    select story id in chat scripted

--- chat_kt_scripted.py
+++ chat_kt_scripted.py
@@ -6,21 +6,22 @@ from muse import signin, chat_scripted_kt, choose_member
 
 
 if __name__ == '__main__':
-	email = input("email: ")
-	password = getpass.getpass("password: ")
+    email = input("email: ")
+    password = getpass.getpass("password: ")
 
-	jwt = signin(email,password)
-	member_id = choose_member(jwt)
-	story_id = 'horse'
+    jwt = signin(email,password)
+    member_id = choose_member(jwt)
+    story_id = input('choose story id options are: horse, school_boy> ')
+    if story_id.lower() not in ['horse','school_boy']:
+        story_id = 'horse'
+    print('using story %s'%story_id)
 
-	while True:
-		user_text = input('[U] ')
-		chat_resp = chat_scripted_kt(user_text,jwt,member_id,story_id)
+    while True:
+        user_text = input('[U] ')
+        chat_resp = chat_scripted_kt(user_text,jwt,member_id,story_id)
 
-		print(chat_resp)
-
-		for line in chat_resp['data']['lines_out']:
-			print('[B]', line['line'])
-		print("YOUR CHOICES ARE:")
-		for c in chat_resp['data']['choices']:
-			print(' - %s'%c)
+        for line in chat_resp['data']['lines_out']:
+            print('[B]', line['line'])
+        print("YOUR CHOICES ARE:")
+        for c in chat_resp['data']['choices']:
+            print(' - %s'%c)
